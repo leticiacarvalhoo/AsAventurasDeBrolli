ded83375201c1aed438c5444faa34fba3950cb65
Let <leticiacarvalhosilva2006@gmail.com>
2024-05-13 17:18:33 -0300

    Primeiro commit

---
+++ src/main/java/Brollito.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author letic
+ */
+public class Brollito {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+        var t = new Teste();
+    }
+}
